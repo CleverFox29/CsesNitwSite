df1484a886846ebce4d216b3d34a75e2fb59421a
CleverFox29 <romilaroy30@gmail.com>
2026-08-19 02:51:37 +0530

    minor title changes

--- src/pages/alumini.tsx
+++ src/pages/alumini.tsx
@@ -29,9 +29,9 @@ export default function Alumini() {
 
     <main className="max-w-7xl mx-auto pt-24 pb-16 px-4">
       <div className="text-center mb-12">
-            <h1 className="text-4xl font-bold ">Our Team</h1>
+            <h1 className="text-4xl font-bold ">Notable Alumini</h1>
             <p className="text-xl text-slate-400 mt-2">
-              Meet the people driving CSES NITW forward.
+              Meet the people creating legacy.
             </p>
           </div>
       <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-6 p-10">
