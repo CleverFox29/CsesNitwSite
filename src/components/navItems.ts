fdc94e1a2565b3998abd6d507376f97086e09561
RamSiddhartha <rs.mamillapalli@gmail.com>
2026-08-26 21:46:00 +0530

    made all the changes in one commit itself, added 7 total commands and made the shell interactive, added a new file in components to store the header links, it was not accessible from the navigation component

---
+++ src/components/navItems.ts
@@ -0,0 +1,8 @@
+export const navItems = [
+  { href: "/", label: "Home", type: "route" as const },
+  { href: "/about", label: "About", type: "route" as const },
+  { href: "/team", label: "Team", type: "route" as const },
+  { href: "/events", label: "Events", type: "route" as const },
+  { href: "/#projects-showcase", label: "Projects", type: "anchor" as const },
+  { href: "/#contact", label: "Contact", type: "anchor" as const },
+];
